c0a90a1a6d4118b73cb39e45b725755c476afc23
Kartikmeena34 <kartikmeena34@gmail.com>
2024-11-30 01:43:58 +0530

    removing import secret.py and hardcode django secret_key

--- shareme/settings.py
+++ shareme/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-from secret import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = Django_Secret_Key
+SECRET_KEY = 'django-insecure-ra*68x-d*3s=c9$e&sv3ki9_=xce^g7r4#j3)3$=qr71n^+q$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
